45f06b0a028e587b920e511ded4087f850ce9e88
durvankur <durvankurp@gmail.com>
2024-07-01 14:34:49 +0530

    sample get post api with model

--- src/modules/app/app.controller.ts
+++ src/modules/app/app.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AppEntity } from './app.entity';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
+  getHello(): Promise<AppEntity[]> {
     return this.appService.getHello();
+  }
+
+  @Post()
+  @HttpCode(201)
+  createData(@Body() request: AppEntity): Promise<AppEntity> {
+    return this.appService.createData(request);
   }
 }
