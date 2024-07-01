ad576b2b9e311b8c61d1c9160acfb72bac61b81a
durvankur <durvankurp@gmail.com>
2024-07-01 22:01:30 +0530

    structual and env changes

---
+++ src/configs/appConfigs.enum.ts
@@ -0,0 +1,4 @@
+export enum AppConfigs {
+  SUPABASE_URL = 'SUPABASE_URL',
+  SUPABASE_ANON_KEY = 'SUPABASE_ANON_KEY',
+}
--- src/modules/app/app.repository.ts
+++ src/modules/app/app.repository.ts
@@ -1,25 +1,16 @@
-import { SupabaseClient, createClient } from '@supabase/supabase-js';
-import { Database } from 'common/types/supabase';
+import { supabase } from 'configs/database';
+import { TABLES } from 'common/constants/tables.enum';
 import { AppEntity } from './app.entity';
-
-const supabase: SupabaseClient<Database> = createClient<Database>(
-  'https://zxcwjeuxxmgoieaumwpa.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp4Y3dqZXV4eG1nb2llYXVtd3BhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTk4MTg4NTIsImV4cCI6MjAzNTM5NDg1Mn0.e70keBSoUWGk3CX7w0dQWwHenb3onBZ2CTszL4h4GfE',
-);
+import { CreateTestDto } from './app.dto';
 
 export class AppRepository {
   async getData(): Promise<AppEntity[]> {
-    const { data, error } = await supabase.from('test').select();
-
-    console.log('error: ', error);
-    console.log('read data: ', data);
+    const { data, error } = await supabase.from(TABLES.TEST).select('*');
     return data;
   }
 
-  async create(request: AppEntity): Promise<AppEntity> {
-    const { data, error } = await supabase.from('test').upsert(request).select();
-
-    console.log('data: ', data);
+  async create(request: CreateTestDto): Promise<AppEntity> {
+    const { data, error } = await supabase.from(TABLES.TEST).upsert(request).select();
     return data && data.length ? data[0] : null;
   }
 }
