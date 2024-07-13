00752d3859cd1473d7a4053b59f99857a1fcc782
durvankur <durvankurp@gmail.com>
2024-07-13 12:21:30 +0530

    validation and logger and constant types

--- src/modules/auth/auth.dto.ts
+++ src/modules/auth/auth.dto.ts
@@ -1,7 +1,13 @@
 import { User } from './auth.entity';
-
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
 export class UserDto {
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(6)
   password: string;
 }
 
