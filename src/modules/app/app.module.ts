b8ae01457d15cd89f50cfea3f8e4e438adadad7c
Kiran <kiran@unitalks.co>
2024-07-17 17:19:45 +0530

    Revert

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -3,12 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { APP_GUARD } from '@nestjs/core';
 import { JwtModule } from '@nestjs/jwt';
 
-import { AuthGuard } from 'commons/guards';
+import { AuthGuard } from '../../commons/guards';
 import { getConfigKeyValue, getDBConfig } from 'configs/index';
-// import { AuthModule } from 'modules/auth/auth.module';
-// import { UsersModule } from 'modules/users/users.module';
+import { AuthModule } from 'modules/auth/auth.module';
+import { UsersModule } from 'modules/users/users.module';
 import { LoggingMiddleware } from 'commons/middlewares';
-// import { CategoriesModule } from 'modules/categories/categories.module';
+import { CategoriesModule } from 'modules/categories/categories.module';
 import { AppConfigs } from 'configs/appConfigs.enum';
 
 import { AppController } from './app.controller';
@@ -21,6 +21,9 @@ import { AppController } from './app.controller';
       secret: getConfigKeyValue(AppConfigs.JWT_SECRETE_KEY),
       signOptions: { expiresIn: '60m' },
     }),
+    AuthModule,
+    UsersModule,
+    CategoriesModule,
   ],
   controllers: [AppController],
   providers: [{ provide: APP_GUARD, useClass: AuthGuard }],
